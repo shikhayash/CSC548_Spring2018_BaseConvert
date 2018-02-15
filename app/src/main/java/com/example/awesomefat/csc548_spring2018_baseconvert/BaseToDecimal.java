1423c79a92de4e48a909056739894475d2cd8e53
Shikha Desai <ssdesai.shikha@gmail.com>
2018-02-15 16:45:58 -0600

    Changes made by shikha

--- app/src/main/java/com/example/awesomefat/csc548_spring2018_baseconvert/BaseToDecimal.java
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_baseconvert/BaseToDecimal.java
@@ -23,21 +23,97 @@ public class BaseToDecimal extends AppCompatActivity
 
     public void base2ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 2");
+        //this.answerTV.setText("Base 2");
+        int tempResult = 0;
+        String s = this.inputET.getText().toString();
+
+        for(int i=s.length()-1, j=0; i >= 0; i--,j++){
+                    int temp = 0;
+                    temp = (int)Math.pow(2,j) * (s.charAt(i) - 48);
+                   tempResult += temp;
+                }
+                this.answerTV.setText("From Binary:  " + tempResult);
     }
 
     public void base8ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 8");
+       // this.answerTV.setText("Base 8");
+        int tempResult = 0;
+        String s = this.inputET.getText().toString();
+
+        for(int i=s.length()-1, j=0; i >= 0; i--,j++){
+            int temp = 0;
+            temp = (int)Math.pow(8,j) * (s.charAt(i) - 48);
+            tempResult += temp;
+        }
+        this.answerTV.setText("From Octal:  " + tempResult);
     }
+    
 
     public void base10ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 10");
+        //this.answerTV.setText("Base 10");
+        int tempResult = 0;
+        String s = this.inputET.getText().toString();
+
+        for(int i=s.length()-1, j=0; i >= 0; i--,j++){
+            int temp = 0;
+            temp = (int)Math.pow(10,j) * (s.charAt(i) - 48);
+            tempResult += temp;
+        }
+        this.answerTV.setText("From Decimal:  " + tempResult);
     }
 
     public void base16ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 16");
+        //this.answerTV.setText("Base 16");
+        int tempResult = 0;
+        String s = this.inputET.getText().toString();
+
+        for(int i=s.length()-1, j=0; i >= 0; i--,j++) {
+            int temp = 0;
+            char c = s.charAt(i);
+            int n = 0;
+
+            if(c=='A' || c == 'a')
+            {
+                n = 10;
+
+            }
+            else if(c=='B' || c == 'b')
+            {
+                n = 11;
+
+            }
+            else if(c=='C' ||  c == 'c')
+            {
+                n = 12;
+
+            }
+            else if(c=='D' || c == 'd')
+            {
+                n = 13;
+
+            }
+            else if(c=='E' || c == 'e')
+            {
+                n = 14;
+
+            }
+            else if(c=='F' || c == 'f')
+            {
+                n = 15;
+
+            }
+            else
+            {
+                n = c - 48;
+            }
+
+            temp = (int) Math.pow(16, j) * n;
+            tempResult += temp;
+
+        }
+        this.answerTV.setText("From Hexadecimal:  " + tempResult);
     }
 }
--- app/src/main/java/com/example/awesomefat/csc548_spring2018_baseconvert/DecimalToBase.java
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_baseconvert/DecimalToBase.java
@@ -24,21 +24,74 @@ public class DecimalToBase extends AppCompatActivity
 
     public void base2ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 2");
+       // this.answerTV.setText("Base 2");
+        String s = this.inputET.getText().toString();
+               int iValue = Integer.parseInt(s);
+                String result = "";
+                while(iValue>0)
+                {
+                    int modValue = 0;
+                    modValue = iValue%2;
+                    iValue = iValue/2;
+                    result = modValue + result;
+                }
+                this.answerTV.setText("Binary: " + result);
     }
 
     public void base8ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 8");
+       // this.answerTV.setText("Base 8");
+
+        String s = this.inputET.getText().toString();
+                int iValue = Integer.parseInt(s);
+                String result = "";
+               while(iValue>0)
+               {
+                   int modValue = 0;
+                   modValue = iValue%8;
+                   iValue = iValue/8;
+                   result = modValue + result;
+               }
+               this.answerTV.setText("octal: " + result);
     }
 
     public void base10ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 10");
+        //this.answerTV.setText("Base 10");
+
+              String s = this.inputET.getText().toString();
+                int iValue = Integer.parseInt(s);
+                String result = "";
+                while(iValue>0)
+                {
+                    int modValue = 0;
+                    modValue = iValue%10;
+                    iValue = iValue/10;
+                    result = modValue + result;
+                }
+                this.answerTV.setText("Decimal: " + result);
     }
 
     public void base16ButtonPressed(View v)
     {
-        this.answerTV.setText("Base 16");
+        //this.answerTV.setText("Base 16");
+
+                String s = this.inputET.getText().toString();
+                int iValue = Integer.parseInt(s);
+                String result = "";
+                char[] charArray = {'A', 'B', 'C', 'D', 'E', 'F'};
+                while(iValue>0)
+                {
+                    int modValue = 0;
+                    modValue = iValue%16;
+                    iValue = iValue/16;
+                    if(modValue>9){
+                        result = charArray[modValue - 10] + result;
+                    } else {
+                        result = modValue + result;
+                    }
+                }
+                this.answerTV.setText("Hexadecimal: " + result);
+
     }
 }
